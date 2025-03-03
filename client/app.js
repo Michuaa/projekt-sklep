fe5beee6a3af8c5d0328c2b3accdd6bd87405f26
Michuaa <fokusi297@gmail.com>
2025-03-03 12:10:48 +0100

    1

---
+++ client/app.js
@@ -0,0 +1,9 @@
+const pierwszy = document.querySelector('#pierwszy')
+const drugi = document.querySelector('#drugi')
+const trzeci = document.querySelector('#trzeci')
+const czwarty = document.querySelector('#czwarty')
+const piąty = document.querySelector('#piąty')
+const szósty = document.querySelector('#szósty')
+    pierwszy.addEventListener('click', ()=>   {
+        document.location.href = 'karta1.html'
+    })
