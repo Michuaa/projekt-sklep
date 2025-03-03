b933f09b83a09e032b7e6e5f06a0f192697641ce
Michuaa <fokusi297@gmail.com>
2025-03-03 12:20:00 +0100

    2

--- client/app.js
+++ client/app.js
@@ -7,3 +7,18 @@ const szósty = document.querySelector('#szósty')
     pierwszy.addEventListener('click', ()=>   {
         document.location.href = 'karta1.html'
     })
+    drugi.addEventListener('click', ()=>   {
+        document.location.href = 'karta2.html'
+    })
+    trzeci.addEventListener('click', ()=>   {
+        document.location.href = 'karta3.html'
+    })
+    czwarty.addEventListener('click', ()=>   {
+        document.location.href = 'karta4.html'
+    })
+    piąty.addEventListener('click', ()=>   {
+        document.location.href = 'karta5.html'
+    })
+    szósty.addEventListener('click', ()=>   {
+        document.location.href = 'karta6.html'
+    })
